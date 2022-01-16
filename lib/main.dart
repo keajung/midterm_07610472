cf0c84a54384623bb454131a3028a0e6ca22ea51
keajung <selanon_w@su.ac.th>
2022-01-16 10:46:20 +0700

    this midterm im dead

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.pink,
       ),
       home: HomePage(),
     );
@@ -39,20 +39,59 @@ class HomePage extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Row(
+        child: Column(
 
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-
           children: [
-            Image.asset('assets/images/pig.png', width: 400.0),
-            SizedBox(width: 8.0),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+
+            Text('PIG WEIGHT',
+                style: TextStyle(
+                    fontSize: 30.0, color: Colors.pink.shade300 ,fontWeight: FontWeight.bold)),
+            Text('CALCULATOR',
+                style: TextStyle(
+                    fontSize: 30.0, color: Colors.pink.shade300, fontWeight: FontWeight.bold)),
+            Text('  ',
+                style: TextStyle(
+                    fontSize: 36.0, color: Colors.pink.shade600, fontWeight: FontWeight.bold)),
+
+            Image.asset('assets/images/pig.png', width: 300.0),
+
+            //SizedBox(width: 8.0),
 
 
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(1),
+                      //border: OutlineInputBorder(),
+                      hintText: 'Enter Length',
+                    ),
+                  )
+                ),
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  width: 50,
+                ),
+                Expanded(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(1),
+                      //border: OutlineInputBorder(),
+                      hintText: 'Enter Girth',
+                    ),
+                  )
+                ),
               ],
             ),
           ],
